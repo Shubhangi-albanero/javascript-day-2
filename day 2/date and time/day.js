fefcc4f4f604263c5b6cdc84381d766daf7c5a99
Shubhangi Gupta <shubhangihu@gmail.com>
2021-09-16 09:32:49 +0530

    dsc

---
+++ day 2/date and time/day.js
@@ -0,0 +1,26 @@
+let d1 = new Date(2012, 1, 20, 3, 12);
+console.log( d1 );
+
+
+function getWeekDay(date) {
+    let days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
+  
+    return days[date.getDay()];
+  }
+  
+  let date = new Date(2014, 0, 3);
+  console.log( getWeekDay(date) );
+
+
+  
+
+  function getLocalDay(date) {
+
+    let day = date.getDay();
+  
+    if (day == 0) { 
+      day = 7;
+    }
+  
+    return day;
+  }
